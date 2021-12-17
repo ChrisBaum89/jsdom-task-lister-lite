ec21d3fe5c39631b7b4ac632aa079cfc1171143a
Chris Baum <Christopher.Baum@valpo.edu>
2021-12-16 21:05:34 -0500

    commit tree eadae4a00035a52b5d405dc8d7dc9dee2b1d0607

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById('create-task-form').addEventListener('submit', function(evnt){
+    evnt.preventDefault()
+    let taskDescription = document.getElementById('new-task-description')
+    let tasksUl = document.getElementById('tasks')
+    let li = document.createElement("li")
+    li.setAttribute("id", taskDescription.value)
+
+    //create reset button
+    let resetButton = document.createElement("button")
+    resetButton.innerHTML = "Complete"
+    resetButton.setAttribute("id", taskDescription.value)
+
+    //append task list with new list element based on task description
+    li.appendChild(document.createTextNode(taskDescription.value + " "))
+    //append new list element with Complete button
+    li.appendChild(resetButton)
+    //append full list
+    tasksUl.appendChild(li)
+    console.log("added task")
+  })
+
+  document.getElementById('tasks').addEventListener('click', function(evnt){
+    let deleteTarget = evnt.target.id
+    let liTarget = document.getElementById(deleteTarget).remove()
+
+  })
 });
